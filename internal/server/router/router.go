a0396fc891667e731549e63e07431c19b803bfdb
Simon <contact@smn.codes>
2025-07-29 19:26:26 +0200

    refactor: rewritten router with stdlib net/http

---
+++ internal/server/router/router.go
@@ -0,0 +1,90 @@
+package router
+
+import (
+	"fmt"
+	"io/fs"
+	"net/http"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+	"gitlab.com/smncd/squishy/internal/filesystem"
+)
+
+// The router context, shared between all methods
+type RouterContext struct {
+	// Squishyfile instance
+	S *filesystem.SquishyFile
+}
+
+// Router is an HTTP request router that uses http.ServeMux, as well as custom context.
+type Router struct {
+	ctx RouterContext
+	mux *http.ServeMux
+}
+
+type HandlerFunc = func(w http.ResponseWriter, r *http.Request, ctx RouterContext)
+
+func New(ctx RouterContext) (*Router, error) {
+	validate := validator.New()
+	err := validate.Struct(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("validation failed: %w", err)
+	}
+
+	rt := &Router{
+		ctx: ctx,
+		mux: http.NewServeMux(),
+	}
+
+	return rt, nil
+}
+
+// Returns the http Multiplexer instance
+func (rt *Router) Mux() *http.ServeMux {
+	return rt.mux
+}
+
+// Handle registers a new request handle with the given path and method.
+func (rt *Router) Handle(method string, path string, handler HandlerFunc) {
+	rt.mux.HandleFunc(fmt.Sprintf("%s %s", method, path), func(w http.ResponseWriter, r *http.Request) {
+		handler(w, r, rt.ctx)
+	})
+}
+
+// Registers a new GET request handle with the given path.
+func (rt *Router) GET(path string, handler HandlerFunc) {
+	rt.Handle("GET", path, handler)
+}
+
+// Registers a new POST request handle with the given path.
+func (rt *Router) POST(path string, handler HandlerFunc) {
+	rt.Handle("POST", path, handler)
+}
+
+// Registers a new PUT request handle with the given path.
+func (rt *Router) PUT(path string, handler HandlerFunc) {
+	rt.Handle("PUT", path, handler)
+}
+
+// Registers a new DELETE request handle with the given path.
+func (rt *Router) DELETE(path string, handler HandlerFunc) {
+	rt.Handle("DELETE", path, handler)
+}
+
+func (rt *Router) StaticFS(path string, fsys fs.FS, errorHandler func(w http.ResponseWriter, r *http.Request)) {
+	path = ensureTrailingSlash(path)
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		filePath := strings.TrimPrefix(r.URL.Path, "/")
+
+		_, err := fs.Stat(fsys, filePath)
+		if err != nil {
+			errorHandler(w, r)
+			return
+		}
+
+		http.FileServer(http.FS(fsys)).ServeHTTP(w, r)
+	})
+
+	rt.mux.Handle(fmt.Sprintf("GET %s", path), handler)
+}

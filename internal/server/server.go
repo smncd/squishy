29d1843bf9a90130e69b66f0ca8c1dd63ebb4b77
Simon <contact@smn.codes>
2025-11-22 00:50:07 +0100

    feat: Only send status codes as error reponse

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,42 +2,30 @@ package server
 
 import (
 	"fmt"
-	"html/template"
-	"io/fs"
 	"log"
 	"net/http"
 
 	"gitlab.com/smncd/squishy/internal/config"
 	"gitlab.com/smncd/squishy/internal/logging"
-	"gitlab.com/smncd/squishy/internal/resources"
 	"gitlab.com/smncd/squishy/internal/router"
 )
 
 type SharedContext struct {
-	cfg           *config.Config
-	routes        *config.Routes
-	errorTemplate *template.Template
-	logger        *log.Logger
+	cfg    *config.Config
+	routes *config.Routes
+	logger *log.Logger
 }
 
 func New(cfg *config.Config, routes *config.Routes, logger *log.Logger) *http.Server {
 	sc := SharedContext{
-		cfg:           cfg,
-		routes:        routes,
-		errorTemplate: template.Must(template.ParseFS(resources.TemplateFS, "templates/error.html")),
-		logger:        logger,
+		cfg:    cfg,
+		routes: routes,
+		logger: logger,
 	}
 
 	router := router.New(sc, logger)
 
-	staticFS, err := fs.Sub(resources.StaticFS, "static")
-	if err != nil {
-		log.Fatalln("error")
-	}
-
 	router.NoRoute(notFoundHandler)
-
-	router.StaticFS("/static/", staticFS)
 
 	router.GET("/", handler)
 
@@ -51,10 +39,6 @@ func New(cfg *config.Config, routes *config.Routes, logger *log.Logger) *http.Se
 
 func notFoundHandler(w http.ResponseWriter, req *http.Request, sc SharedContext) {
 	w.WriteHeader(http.StatusNotFound)
-	sc.errorTemplate.Execute(w, resources.ErrorTemplateData{
-		Title:       "Not Found",
-		Description: "The link you've accessed does not exist",
-	})
 }
 
 func handler(w http.ResponseWriter, r *http.Request, sc SharedContext) {
@@ -62,18 +46,11 @@ func handler(w http.ResponseWriter, r *http.Request, sc SharedContext) {
 
 	err := sc.routes.Refetch()
 	if err != nil {
-		data := resources.ErrorTemplateData{
-			Title:       "Welp, that's not good",
-			Description: "There's been an error on our end, please check back later",
-		}
 		if sc.cfg.Debug {
-			data.Error = err.Error()
-
 			logging.Debug(sc.logger, "error refetching routes: %s", err)
 		}
 
 		w.WriteHeader(http.StatusInternalServerError)
-		sc.errorTemplate.Execute(w, data)
 		return
 	}
 
